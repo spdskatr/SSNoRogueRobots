e495c835042ca1e689d22c8243f7fa0968108895
spdskatr <tunan.shi@gmail.com>
2017-06-23 20:01:32 +1000

    0.17.1
    Updated so I don't have to change method name of compiler generated methods every rebuild - Thanks erdelf

--- Source/Patches.cs
+++ Source/Patches.cs
@@ -3,6 +3,8 @@ using RimWorld.BaseGen;
 using Harmony;
 using System.Reflection;
 using RimWorld;
+using System;
+using System.Runtime.CompilerServices;
 /* Below lies the usings for the transpiler to work
 using System.Reflection.Emit;
 using System.Collections.Generic;
@@ -10,13 +12,29 @@ using System.Linq;
 */
 namespace NoRogueRobots
 {
-    [StaticConstructorOnStartup, HarmonyPatch(typeof(Building_CrashedShipPart), "<TrySpawnMechanoids>m__4A2")]
+    [StaticConstructorOnStartup, HarmonyPatch]
     public static class Patches
     {
+        public const BindingFlags compilerGeneratedBindingFlags = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
         static Patches()
         {
             HarmonyInstance.Create("com.spdskatr.NoRogueRobots.patches").PatchAll(Assembly.GetExecutingAssembly());
-            Log.Message("SS No Rogue Robots initialized. Patched:\nBuilding_CrashedShipPart.<TrySpawnMechanoids>m__4A2 (Non-destructive postfix)\nSymbolResolver_RandomMechanoidGroup.<Resolve>m__271 (Non-destructive postfix)\n\n");
+            Log.Message("SS No Rogue Robots initialized. Patched:\nBuilding_CrashedShipPart.<TrySpawnMechanoids>m__*** (Non-destructive postfix, uses method get_IsMechanoid)\nSymbolResolver_RandomMechanoidGroup.<Resolve>m__*** (Non-destructive postfix, uses method get_IsMechanoid)\n\n");
+        }
+        /// <summary>
+        /// Thanks to erdelf for that quick tip about getting compiler generated methods cross platform and build.
+        /// </summary>
+        static MethodInfo TargetMethod()
+        {
+            var methods = typeof(Building_CrashedShipPart).GetMethods(compilerGeneratedBindingFlags);
+            foreach (var method in methods)
+            {
+                if (method.HasAttribute<CompilerGeneratedAttribute>() && method.GetParameters()[0].ParameterType == typeof(PawnKindDef))
+                {
+                    return method;
+                }
+            }
+            throw new Exception("Method not found for SS No Rogue Robots typeof(Building_CrashedShipPart)");
         }
         static void Postfix(PawnKindDef def, ref bool __result)
         {
@@ -46,9 +64,21 @@ namespace NoRogueRobots
             return def.defaultFactionType != Faction.OfMechanoids.def;
         }
     }
-    [HarmonyPatch(typeof(SymbolResolver_RandomMechanoidGroup), "<Resolve>m__271")]
+    [HarmonyPatch]
     public static class Patches_2
     {
+        static MethodInfo TargetMethod()
+        {
+            var methods = typeof(SymbolResolver_RandomMechanoidGroup).GetMethods(Patches.compilerGeneratedBindingFlags);
+            foreach (var method in methods)
+            {
+                if (method.ReturnType == typeof(bool) && method.HasAttribute<CompilerGeneratedAttribute>())
+                {
+                    return method;
+                }
+            }
+            throw new Exception("Method not found for SS No Rogue Robots typeof(SymbolResolver_RandomMechanoidGroup)");
+        }
         static void Postfix(PawnKindDef kind, ref bool __result)
         {
             if (Patches.ShouldNotBelong(kind)) __result = false;
